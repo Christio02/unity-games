0edb9023d52f10c998e93871e99ba660a4260498
Christopher Høe <christopher.hoee@gmail.com>
2023-02-04 21:29:46 +0100

    Added moving car obstacles, more box obstacles and tried adding camera switch

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,20 +6,36 @@ public class FollowPlayer : MonoBehaviour
 {
     // variables
     public GameObject player; // gets player object
-    private Vector3 offset = new Vector3(0, 7, -10); // sets camera default postion to follow player
+    public GameObject cam1;
+    public GameObject cam2;
+    private Vector3 offset1 = new Vector3(0, 7, -10); // sets camera default postion to follow player
 
+    private Vector3 firstP = new Vector3(0, 5, 1);
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam1.SetActive(true);
+        cam2.SetActive(false);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         // Offset the camera whenever player is moving
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + offset1;
+        
+        if (Input.GetButtonDown("Cam1")) { 
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+        }
+
+        if (Input.GetButtonDown("Cam2")) {
+            offset1 = firstP;
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+        }
     }
 }
